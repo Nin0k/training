0d0239398668a98c85960d6b6ffede74e7cf3ac3
Nin0k <nimfodora95@mail.ru>
2020-05-21 01:39:00 +0400

    The first version of the task

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -10,7 +10,34 @@ namespace Task_10
     {
         static void Main(string[] args)
         {
+          
+            Random rand = new Random();
 
+            int[,] array_2d = new int[5, 5];
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                        array_2d[i, j] = rand.Next(-30, 30);
+                        Console.Write(" " + array_2d[i, j]);
+                }
+                Console.WriteLine(" ");
+            }
+            int summ = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    int parity = (i + j) % 2; 
+                        if (parity == 0)
+                        {
+                            summ += array_2d[i, j];
+                        }
+                }
+            }
+            Console.Write("Сумма элементов массива, стоящих на чётных позициях: " + summ);
+            Console.ReadLine();
         }
     }
 }
--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -10,6 +10,27 @@ namespace Task_9
     {
         static void Main(string[] args)
         {
+            Console.Write("Массив:");
+
+            int[] array = new int[20];
+            Random rand = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-99, 99);
+                Console.Write(" " + array[i]);
+            }
+            Console.WriteLine();
+            int summ = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > 0)
+                {
+                    summ += array[i];
+                }
+            }
+            Console.Write("Cумма неотрицательных элементов: " + summ);
+            Console.ReadLine();
         }
     }
 }
