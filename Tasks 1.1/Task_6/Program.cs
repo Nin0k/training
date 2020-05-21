d984c53d25d23f013e3668c49dff44df8d7936ac
Nin0k <nimfodora95@mail.ru>
2020-05-21 23:30:26 +0400

    Task 1.1 THE MAGNIFICENT TEN

--- Tasks 1.1/Task_6/Program.cs
+++ Tasks 1.1/Task_6/Program.cs
@@ -8,16 +8,44 @@ namespace Task_6
 {
     class Program
     {
+        [Flags]
+        enum Font : byte
+        {
+            None = 0,
+            Bold = 1,
+            Italic = 2,
+            Underline = 4
+        }
         static void Main(string[] args)
         {
-            /*
-             * 
-             *Для форматирования текста надписи можно использовать различные начертания: полужирное,
-             * курсивное и подчёркнутое, а также их сочетания. Предложите способ хранения информации
-             * о форматировании текста надписи и напишите программу, которая позволяет устанавливать 
-             * и изменять начертание 
-             * 
-             */
+            var type_font = Font.None;
+            
+            while (true)
+            {
+                Console.WriteLine("Введите:");
+                Console.WriteLine("     1: bold");
+                Console.WriteLine("     2: italic");
+                Console.WriteLine("     3: underline");
+                int font_new = int.Parse(Console.ReadLine());
+
+                switch (font_new)
+                {
+                    case 1:
+                        type_font = type_font ^ Font.Bold;
+                        break;
+                    case 2:
+                        type_font = type_font ^ Font.Italic;
+                        break;
+                    case 3:
+                        type_font = type_font ^ Font.Underline;
+                        break;
+                    default:
+                        Console.WriteLine("Вы ввели неизвестное число");
+                        break;
+                }
+                Console.WriteLine("Параметры надписи: " + type_font);
+            }
+            
         }
     }
 }
