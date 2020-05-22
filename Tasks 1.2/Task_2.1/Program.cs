52c0b51fd03d49db19738ac5ac00a91a05bd147e
Nin0k <nimfodora95@mail.ru>
2020-05-22 21:49:11 +0400

    Tasks 1.1.1 - 1.2.1

---
+++ Tasks 1.2/Task_2.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._1
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+          //  string str = "Викентий хорошо отметил день рождения: покушал пиццу, посмотрел кино, пообщался со студентами в чате.";
+            Console.Write("Введите строку:");
+            string str = Console.ReadLine();
+            string[] words = str.Split(new[] { ' ', '!', '?', ':', ';', ',', '.', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            int average = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+                average += words[i].Length;
+            }
+            average /= words.Length;
+            Console.WriteLine("Средняя длина слова: " + average);
+            
+            Console.ReadLine();
+        }
+    }
+}
