9727a82fff9595e3c1661975041cc72b6a758815
Nin0k <nimfodora95@mail.ru>
2020-06-05 18:59:38 +0400

    Task completed 2.1.1

---
+++ Tasks_2/2.1.1._CUSTOM STRING/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChangedString;
+
+
+namespace Task_2._1._1_CUSTOM_STRING
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            СhangedString example = new СhangedString("hello");
+            СhangedString example2 = new СhangedString("goodbye");
+
+            example.GetInfo();
+            Console.WriteLine();
+
+            Console.WriteLine($"Строки равны? {example.СompareStrings(example2)}");
+           
+            СhangedString ex = example + example2;
+            Console.WriteLine($"Конкатенация строк: {ex.NoArray()}");
+           
+
+            Console.WriteLine($"Индекс первого вхождения символа: {example.SearchChar('l', 1)}");
+            Console.WriteLine($"Индекс последнего вхождения символа: {example.LastSearchChar('l')}");
+
+            Console.WriteLine($"Идут ли два одинаковых символа подряд? {example.RepeatSymbol('l')}");
+          
+            example2.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
