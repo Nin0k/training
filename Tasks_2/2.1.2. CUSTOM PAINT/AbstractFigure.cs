a51a4f676c8fff4b34f6022e24a956fd03ea8cdd
Nin0k <nimfodora95@mail.ru>
2020-06-20 14:35:16 +0400

    Task completed 2.1.2 v5

--- Tasks_2/2.1.2. CUSTOM PAINT/AbstractFigure.cs
+++ Tasks_2/2.1.2. CUSTOM PAINT/AbstractFigure.cs
@@ -8,8 +8,34 @@ namespace _2._1._2.CUSTOM_PAINT
 {
     abstract class AbstractFigure
     {
-        public double x;
-        public double y;
+        
+        private double _X { get; set; }
+        private double _Y { get; set; }
+
+        public double x
+        {
+            get
+            {
+                return _X;
+            }
+
+            set
+            {
+                _X = value;
+            }
+        }
+        public double y
+        {
+            get
+            {
+                return _Y;
+            }
+
+            set
+            {
+                _Y = value;
+            }
+        }
 
         public const int redouble = 2;
 
