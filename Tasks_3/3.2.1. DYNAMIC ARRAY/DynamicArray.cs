e5e6bcbfc37cbeb040b1e0b41cf51f4e5dd54fe7
Nin0k <nimfodora95@mail.ru>
2020-07-12 12:21:12 +0400

    Added task 3.2.1

---
+++ Tasks_3/3.2.1. DYNAMIC ARRAY/DynamicArray.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _3._2._1.DYNAMIC_ARRAY
+{
+    class DynamicArray<T> : IEnumerable<T>
+    {
+        private const int INITIAL_CAPACITY = 8;
+        protected T[] array;
+
+        public DynamicArray()
+        {
+            array = new T[INITIAL_CAPACITY];
+
+        }
+
+        public DynamicArray(int newCapacity)
+        {
+            array = new T[newCapacity];
+        }
+
+        public DynamicArray(IEnumerable<T> arrayCollection)
+        {
+            array = new T[arrayCollection.Count()];
+
+            AddRange(arrayCollection);
+        }
+
+        public void Add(T element)
+        {
+
+            if (Length == Capacity)
+            {
+
+                ExpandArray(Capacity * 2);
+            }
+
+            array[Length++] = element;
+
+        }
+
+        public void AddRange(IEnumerable<T> elements)
+        {
+
+
+            if (Length + elements.Count() > Capacity)
+            {
+                ExpandArray(Capacity + elements.Count());
+            }
+            foreach (var i in elements)
+            {
+                array[Length++] = i;
+            }
+        }
+        public void ExpandArray(int capacityExpansion)
+        {
+
+            if (Length < 1)
+            {
+                array = new T[capacityExpansion];
+            }
+            else
+            {
+                T[] newArray = new T[capacityExpansion];
+                array.CopyTo(newArray, 0);
+                array = newArray;
+
+            }
+
+        }
+
+        public bool Remove(T element)
+        {
+
+            for (int i = 0; i < Length; i++)
+            {
+                int sourceIndex = i + 1;
+
+                if (array[i].Equals(element))
+                {
+                    Array.Copy(array, sourceIndex, array, i, Length - sourceIndex);
+
+                    Length--;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public void RemoveAt(int index)
+        {
+            if (index >= Length)
+            {
+                throw new IndexOutOfRangeException("The index cannot go beyond the array boundary");
+            }
+
+            int shiftStart = index + 1;
+
+            if (shiftStart < Length)
+            {
+                Array.Copy(array, shiftStart, array, index, Length - shiftStart);
+                Length--;
+            }
+
+
+        }
+        public bool Insert(T element, int index)
+        {
+            if (index > 0 || index > Length)
+            {
+                if (Length == Capacity)
+                {
+                    ExpandArray(Capacity * 2);
+                }
+                Array.Copy(array, index, array, index + 1, Length - index);
+
+                array[index] = element;
+
+                Length++;
+                return true;
+            }
+            throw new ArgumentOutOfRangeException("The index cannot go beyond the array boundary");
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in array)
+            {
+                yield return item;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public int Length { get; set; }
+        public int Capacity
+        {
+            get
+            {
+
+                return array.Length;
+            }
+            set
+            {
+
+                array = new T[value];
+            }
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                if (index >= array.Length || index <= -array.Length)
+                {
+                    throw new IndexOutOfRangeException("The index cannot go beyond the array boundary");
+                }
+                array[index] = value;
+            }
+        }
+        public object Clone()
+        {
+            T[] newArray = new T[Capacity];
+
+            Array.Copy(array, newArray, Length);
+
+            return new DynamicArray<T>(newArray);
+        }
+    }
+}
---
+++ Tasks_3/3.2.1. DYNAMIC ARRAY/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+
+namespace _3._2._1.DYNAMIC_ARRAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] collecttion1 = { 1, 2, 4, 6, 8, 10 };
+            int[] collecttion2 = { 3, 5, 7, 9};
+
+            DynamicArray<int> array = new DynamicArray<int>(collecttion1);
+            
+            Console.WriteLine(new string('-', 45));
+            Console.WriteLine("Примеры с коллекциями:");
+            Console.WriteLine(new string('-', 45));
+            Console.WriteLine("Collection initial:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Capacity collection initial: " + array.Capacity);
+            Console.WriteLine("Length collection initial: " + array.Length);
+            Console.WriteLine(new string('-', 35));
+
+            array.AddRange(collecttion2);
+
+            Console.WriteLine("");
+            Console.WriteLine("Capacity collection with AddRange(collecttion2 ): " + array.Capacity);
+            Console.WriteLine("Length collection with AddRange(collecttion2 ): " + array.Length);
+            Console.WriteLine(new string('-', 35));
+
+            array.Remove(7);
+
+            Console.WriteLine("");
+            Console.WriteLine("Capacity collection with Remove(7): " + array.Capacity);
+            Console.WriteLine("Length collection with Remove(7): " + array.Length);
+            Console.WriteLine(new string('-', 35));
+
+            array.Insert(22, 3);
+            array.Insert(27, 6);
+
+            Console.WriteLine("Insert(22, 3) & Insert(27, 6):");
+            Console.WriteLine(new string('-', 35));
+            Console.WriteLine("Collection final:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Capacity collection final: " + array.Capacity);
+            Console.WriteLine("Length collection final: " + array.Length);
+            Console.WriteLine(new string('-', 45));
+
+            DynamicArray<int> array2 = new DynamicArray<int>() { 1, 2, 4, 6, 8, 10, 2, 4, 6, 8, 10 };
+
+            Console.WriteLine("Примеры с массивом:");
+            Console.WriteLine(new string('-', 45));
+            Console.WriteLine("Array initial:");
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write(array2[i] + " ");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Capacity array initial: " + array2.Capacity);
+            Console.WriteLine("Length array initial: " + array2.Length);
+            Console.WriteLine(new string('-', 35));
+
+            array2.Add(7);
+            
+            Console.WriteLine("Capacity array with Add(7): " + array2.Capacity);
+            Console.WriteLine("Length array with Add(7): " + array2.Length);
+            Console.WriteLine(new string('-', 35));
+
+            array2.Remove(2);
+
+            Console.WriteLine("Capacity array with Remove(2):" + array2.Capacity);
+            Console.WriteLine("Length array with Remove(2): " + array2.Length);
+            Console.WriteLine(new string('-', 35));
+
+
+            array2.Insert(11, 5);
+
+            Console.WriteLine("Insert(11, 5):");
+            Console.WriteLine(new string('-', 35));
+            Console.WriteLine("Array final:" );
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write(array2[i] + " ");
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine(new string('-', 35));
+            Console.WriteLine("Capacity array final: " + array2.Capacity);
+            Console.WriteLine("Length array final: " + array2.Length);
+            
+            Console.ReadLine();
+        }
+      
+    }
+
+    
+}
