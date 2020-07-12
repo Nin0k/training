158711028a19056565f3bab6823dfa0f18e0c8d0
Nin0k <nimfodora95@mail.ru>
2020-07-12 22:05:39 +0400

    Added task 3.3.2

---
+++ Tasks_3/3.3.2. SUPER STRING/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _3._3._2.SUPER_STRING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToCheck = "Тысячадевятьсотвосьмидесятидевятимиллиметровый";
+            
+            Console.WriteLine($"Слово {stringToCheck} на {stringToCheck.CheckForLanguage()} языке");
+
+            string stringToCheck2 = "Incomprehensibilities";
+            Console.WriteLine($"Слово {stringToCheck2} на {stringToCheck2.CheckForLanguage()} языке");
+
+            string stringToCheck3 = "0123456789";
+            Console.WriteLine($"Слово {stringToCheck3} на {stringToCheck3.CheckForLanguage()} языке");
+
+
+            string stringToCheck4 = "Комната13f";
+            Console.WriteLine($"Слово {stringToCheck4} на {stringToCheck4.CheckForLanguage()} языке");
+
+
+
+            Console.ReadLine();
+        }
+    }
+    
+}
---
+++ Tasks_3/3.3.2. SUPER STRING/StringExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3._2.SUPER_STRING
+{
+    public static class StringExtensions
+    {
+        public static string CheckForLanguage(this string stringToCheck)
+        {
+            stringToCheck = stringToCheck.ToLower();
+
+            int RuCodeBegin = char.ConvertToUtf32("а", 0) - 1;
+            int RuCodeEnd = char.ConvertToUtf32("я", 0) + 1;
+            int RuCodeE = char.ConvertToUtf32("ё", 0);
+
+            int EnCodeBegin = char.ConvertToUtf32("a", 0) - 1;
+            int EnCodeEnd = char.ConvertToUtf32("z", 0) + 1;
+
+            int NumCodeBegin = char.ConvertToUtf32("0", 0) - 1;
+            int NumCodeEnd = char.ConvertToUtf32("9", 0) + 1;
+
+            if (stringToCheck.All(s => s > RuCodeBegin && s < RuCodeEnd || s == RuCodeE))
+            {
+                return "Russian";
+            }
+            else if (stringToCheck.All(s => s > EnCodeBegin && s < EnCodeEnd))
+            {
+                return "English";
+            }
+            else if (stringToCheck.All(s => s > NumCodeBegin && s < NumCodeEnd))
+            {
+                return "Number";
+            }
+            return "Mixed";
+
+        }
+    }
+}
