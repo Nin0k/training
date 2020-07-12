9411f3aef8cf33a3b8f32fc885d6ed380da31ccb
Nin0k <nimfodora95@mail.ru>
2020-07-12 12:22:18 +0400

    Added task 3.3.1

---
+++ Tasks_3/3.3.1. SUPER ARRAY/ArrayExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3._1.SUPER_ARRAY
+{
+    public static class ArrayExtensions
+    {
+        public static double[] Modification(this double[] mass, Func<double, double> function)
+        {
+            if (function == null)
+            {
+                throw new NullReferenceException("Cannot call an empty delegate");
+            }
+
+            double[] newAMass = new double[mass.Length];
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                newAMass[i] = function(mass[i]);
+            }
+
+            return newAMass;
+        }
+
+        public static double SumElements(this double[] mass)
+        {
+            return mass.Sum();
+        }
+
+        public static double AverageArray(this double[] mass)
+        {
+            return mass.Average();
+        }
+
+        public static double RepeatElements(this double[] mass)
+        {
+            double element = mass[0];
+            int repeat;
+            int maxRepeat = 1;
+
+            for (int i = 0; i < mass.Length; i++)
+            {
+                repeat = mass.Count(e => e == mass[i]);
+
+                if (repeat > maxRepeat)
+                {
+                    maxRepeat = repeat;
+                    element = mass[i];
+
+                }
+            }
+
+            if (maxRepeat == 1)
+            {
+                Console.WriteLine("Все элементы повторяются только по одному разу");
+            }
+            return element;
+        }
+
+    }
+}
---
+++ Tasks_3/3.3.1. SUPER ARRAY/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3._1.SUPER_ARRAY
+{
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] mass = new double[] { 2, 6, 9, 15, 6, 3, 13 };
+            double[] mass1;
+            double[] mass2;
+            double[] mass3;
+            Console.WriteLine($"Исходный массив: ");
+            foreach (var i in mass)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Сумма элементов: {mass.SumElements()}");
+
+            Console.WriteLine($"Среднее значение: {mass.AverageArray()}");
+
+             Console.WriteLine($"Самый часто повторяемый элемент: {mass.RepeatElements()}");
+           
+            mass1 = mass.Modification((a)=> a * a);
+
+
+            Console.WriteLine($"Исходный массив возведенный в квадрат: ");
+            foreach (var i in mass1)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            mass2 = mass.Modification((a) => a * 3);
+
+            Console.WriteLine($"Исходный массив умноженный на 3: ");
+            foreach (var i in mass2)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            mass3 = mass.Modification((a) => a / 2);
+
+            Console.WriteLine($"Исходный массив деленный на 2: ");
+            foreach (var i in mass3)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+   
+}
