3999dcf26f484f92f7e91d936d60c342800c5f5d
Nin0k <nimfodora95@mail.ru>
2020-08-12 17:23:31 +0400

    Added task 5.1

---
+++ Tasks_5/5.1 CHAR REMOVER/index.js
@@ -0,0 +1,35 @@
+var originalString = "У попа была собака, он её любил!";
+console.log(originalString);
+
+function charRemover(str, repeatedChar) {
+
+    var rezult = [];
+
+    for(i=0; i < str.length; i++){
+        if(repeatedChar.indexOf(str[i]) == -1){
+
+            rezult.push(str[i]);
+        }
+    }
+    rezultString = rezult.join('');
+    console.log(rezultString);
+}
+
+function changeStr(str) { 
+   
+    var arrWords =  str.split(" ");
+    var repeatedChar = [];
+
+    for(i=0; i <arrWords.length; i++){
+        for(j=0; j <arrWords[i].length; j++) {
+
+            if (arrWords[i].indexOf(arrWords[i][j]) !== arrWords[i].lastIndexOf(arrWords[i][j]) ) {
+
+                repeatedChar.push(arrWords[i][j]);
+            }
+        }
+    }
+    charRemover(str, repeatedChar)
+  }
+  
+  changeStr(originalString);
