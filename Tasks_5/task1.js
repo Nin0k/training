9e47bd6de9d522801d18d510a2c2bc70337cf1ed
Nin0k <nimfodora95@mail.ru>
2020-08-14 13:13:37 +0400

    Task 5.1 edited

--- Tasks_5/task1.js
+++ Tasks_5/task1.js
@@ -1,27 +1,27 @@
-var originalString = "У попа была собака, он её любил!";
+let originalString = "У попа была собака, он её любил!";
 console.log(originalString);
 
 function charRemover(str, repeatedChar) {
 
-    var rezult = [];
+    let rezult = [];
 
-    for(i=0; i < str.length; i++){
+    for(let i=0; i < str.length; i++){
         if(repeatedChar.indexOf(str[i]) == -1){
 
             rezult.push(str[i]);
         }
     }
-    var rezultString = rezult.join('');
+    let rezultString = rezult.join('');
     console.log(rezultString);
 }
 
 function changeStr(str) { 
    
-    var arrWords =  str.split(" ");
-    var repeatedChar = [];
+    let arrWords =  str.split(" ");
+    let repeatedChar = [];
 
-    for(i=0; i <arrWords.length; i++){
-        for(j=0; j <arrWords[i].length; j++) {
+    for(let i=0; i <arrWords.length; i++){
+        for(let j=0; j <arrWords[i].length; j++) {
 
             if (arrWords[i].indexOf(arrWords[i][j]) !== arrWords[i].lastIndexOf(arrWords[i][j]) ) {
 
